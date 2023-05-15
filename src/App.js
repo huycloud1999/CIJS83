281c8e7c15fa734e2d94cb5210e3a2a5646e8ca9
LongVuong <longvuong1920@gmail.com>
2023-05-15 23:54:14 +0700

    test branch

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import SlidingForm from "./components/SlidingForm/SlidingForm";
 import SignIn from "./components/SignIn/SignIn";
+
 function App() {
   return (
     <div>
-      <div className="container" style={{ display: "flex", flexDirection: "row"}}>
-        <div className="nav" style={{ backgroundColor: "red", width: "5%", position: "fixed", height: "100%"}}>
+      <div className="container" style={{ display: "flex", flexDirection: "row" }}>
+        <div className="nav"
+          style={{
+            backgroundColor: "red",
+            width: "5%",
+            position: "fixed",
+            height: "90%"
+          }}>
           <Navbar />
         </div>
         <div className="headerMain" style={{ width: "95%", height: "100%", marginLeft: "5%" }}>
@@ -21,9 +28,9 @@ function App() {
         </div>
       </div>
       <div className="playBar" style={{ backgroundColor: "pink", width: "100vw", height: "10%", position: "fixed", bottom: 0 }}>
-        <PlayBar/>
+        <PlayBar />
       </div>
-         {/* <SlidingForm /> */}
+      {/* <SlidingForm /> */}
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from './Navbar.module.css'
+import Icon from '../../shared/Icon'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={styles.NavBar}>
+      <div className={styles.Logo}>
+        Logo
+      </div>
+      <div className={styles.NavIcons}>
+        <button className={styles.NavBtn}>
+        <Icon><i class="fa-solid fa-house" aria-hidden='true' style={{ fontSize: 24 }}></i></Icon>
+        </button>
+        <button className={styles.NavBtn}>
+        <Icon><i class="fa-solid fa-record-vinyl" aria-hidden='true' style={{ fontSize: 24 }}></i></Icon>
+        </button>
+        <button className={styles.NavBtn}>
+        <Icon><i class="fa-solid fa-user-secret" aria-hidden='true' style={{ fontSize: 24 }}></i></Icon>
+        </button>
+        <button className={styles.NavBtn}>
+        <Icon><i class="fa-solid fa-radio" aria-hidden='true' style={{ fontSize: 24 }}></i></Icon>
+        </button>
+      </div>
+    </div>
   )
 }
 

6321547e5f68c13ab7ef34f0b67e49549c722f9e
LongVuong <longvuong1920@gmail.com>
2023-05-16 02:18:44 +0700

    fix color

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Background from './public/image/azurlane.jpg';
 import Navbar from "./components/Navbar/Navbar";
 import PlayBar from "./components/PlayBar/PlayBar";
 import Header from "./components/Header/Header";
@@ -7,11 +8,11 @@ import SignIn from "./components/SignIn/SignIn";
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: '#16161a' }}>
       <div className="container" style={{ display: "flex", flexDirection: "row" }}>
         <div className="nav"
           style={{
-            backgroundColor: "red",
+            // backgroundColor: "#16161a",
             width: "5%",
             position: "fixed",
             height: "90%"
@@ -19,10 +20,10 @@ function App() {
           <Navbar />
         </div>
         <div className="headerMain" style={{ width: "95%", height: "100%", marginLeft: "5%" }}>
-          <div style={{ backgroundColor: "green", height: "10%", position: "fixed", top: 0, width: "100%" }}>
+          <div style={{ height: "10%", position: "fixed", top: 0, width: "100%", backgroundColor: '#16161a' }}>
             <Header />
           </div>
-          <div style={{ backgroundColor: "violet", marginTop: "5%" }}>
+          <div style={{ marginTop: "5%", color: '#94a1b2' }}>
             <Main />
           </div>
         </div>
